abbc9d98e3547335df8b76c8650420109911431a
sskys18 <123645816+sskys18@users.noreply.github.com>
2023-11-28 17:28:47 +0900

    mission8
    git 오류로 인한 재생성

---
+++ spring/src/main/java/umc/study/domain/Review.java
@@ -0,0 +1,30 @@
+package umc.study.domain;
+
+import lombok.*;
+import umc.study.domain.common.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+
+public class Review extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long review_id;
+
+    private String title;
+
+    private Float score;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+}
